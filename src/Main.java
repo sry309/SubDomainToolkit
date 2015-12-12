e4649515360e7764829ff470abd23e40cee392fe
orange <orange@lightmoon.local>
2015-12-12 12:39:07 +0800

    update fxml

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("newui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
         primaryStage.setTitle("SubdomainToolkit Alpha v0.1");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.setResizable(false);
